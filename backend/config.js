f7194f403ad61377020ba5d4bbaf48d88e5e351f
M.Syahrindra <syahrindraakbar09@gmail.com>
2024-07-30 22:42:22 +0700

    create node js project, route, add mongodb n mongoose to node js

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL=
+'mongodb+srv://root:book-store-db@book-store.ohru3af.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store'
